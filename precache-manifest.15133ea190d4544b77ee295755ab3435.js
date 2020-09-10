5b61bc437e97bb3cb7ccfaa811fa5879c45e7e24
adiwawan90 <adi.wawan90@gmail.com>
2020-09-10 08:37:41 +0700

    Updates

---
+++ precache-manifest.15133ea190d4544b77ee295755ab3435.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ab5bbb952d3ed20a0006b66f86834b",
+    "url": "/react-repo/index.html"
+  },
+  {
+    "revision": "6cffa04c1096aa904e6c",
+    "url": "/react-repo/static/css/main.7080421b.chunk.css"
+  },
+  {
+    "revision": "864c65d5cee24ee9d9f0",
+    "url": "/react-repo/static/js/2.20e4c694.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-repo/static/js/2.20e4c694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cffa04c1096aa904e6c",
+    "url": "/react-repo/static/js/main.27271cd7.chunk.js"
+  },
+  {
+    "revision": "2b0ea6e41b7b3b9c80c4",
+    "url": "/react-repo/static/js/runtime-main.433270e8.js"
+  }
+]);
